098564451409f45a022ec66367b9dca19e078e94
daniloarodrigues <danilo.de.rodrigues@accenture.com>
2019-09-28 17:12:23 -0300

    Criação do módulo prontuario e prontuario-routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProntuarioComponent } from './modules/prontuario/prontuario.component';
 import { LoginComponent } from './modules/prontuario/components/login/login.component';
 import { HomeComponent } from './modules/prontuario/components/home/home.component';
 import { NavComponent } from './modules/prontuario/components/nav/nav.component';
@@ -12,11 +11,11 @@ import { AnamneseComponent } from './modules/prontuario/components/anamnese/anam
 import { EventosComponent } from './modules/prontuario/components/eventos/eventos.component';
 import { HistoricoClinicoComponent } from './modules/prontuario/components/historico-clinico/historico-clinico.component';
 import { GestaoDeUsuariosComponent } from './modules/prontuario/components/gestao-de-usuarios/gestao-de-usuarios.component';
+import { ProntuarioModule } from './modules/prontuario/prontuario.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProntuarioComponent,
     LoginComponent,
     HomeComponent,
     NavComponent,
@@ -28,7 +27,8 @@ import { GestaoDeUsuariosComponent } from './modules/prontuario/components/gesta
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ProntuarioModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/modules/prontuario/prontuario.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProntuarioRoutingModule } from './prontuario-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ProntuarioRoutingModule
+  ]
+})
+export class ProntuarioModule { }
