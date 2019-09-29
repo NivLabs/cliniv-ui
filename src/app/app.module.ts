844637abea7dc2464d2066d858751dd4de948a1a
Vinícios Rodrigues <vinicios.araujo@linx.com.br>
2019-09-29 10:04:19 -0300

    Corrigindo imports dos modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './modules/prontuario/components/login/login.component';
-import { HomeComponent } from './modules/prontuario/components/home/home.component';
-import { NavComponent } from './modules/prontuario/components/nav/nav.component';
-import { ProfileComponent } from './modules/prontuario/components/profile/profile.component';
-import { AnamneseComponent } from './modules/prontuario/components/anamnese/anamnese.component';
-import { EventosComponent } from './modules/prontuario/components/eventos/eventos.component';
-import { HistoricoClinicoComponent } from './modules/prontuario/components/historico-clinico/historico-clinico.component';
-import { GestaoDeUsuariosComponent } from './modules/prontuario/components/gestao-de-usuarios/gestao-de-usuarios.component';
+import { LoginComponent } from './modules/prontuario/pages/login/login.component';
+import { HomeComponent } from './modules/prontuario/pages/home/home.component';
+import { NavComponent } from './modules/prontuario/pages/nav/nav.component';
+import { ProfileComponent } from './modules/prontuario/pages/profile/profile.component';
+import { AnamneseComponent } from './modules/prontuario/pages/anamnese/anamnese.component';
+import { EventosComponent } from './modules/prontuario/pages/eventos/eventos.component';
+import { HistoricoClinicoComponent } from './modules/prontuario/pages/historico-clinico/historico-clinico.component';
+import { GestaoDeUsuariosComponent } from './modules/prontuario/pages/gestao-de-usuarios/gestao-de-usuarios.component';
 import { ProntuarioModule } from './modules/prontuario/prontuario.module';
 
 @NgModule({
