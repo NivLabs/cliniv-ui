9c594a40c7760d72126dfcf962ec7557bcfbea69
Vinícios <viniciosarodrigues@gmail.com>
2019-10-14 23:47:46 -0300

    Adicionando template Angular 8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,73 +1,45 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
+
+import { AppRoutingModule } from './app.routing';
+import { ComponentsModule } from './components/components.module';
+
 import { AppComponent } from './app.component';
-import { LoginComponent } from './modules/prontuario/pages/login/login.component';
-import { HomeComponent } from './modules/prontuario/pages/home/home.component';
-import { NavComponent } from './modules/prontuario/pages/nav/nav.component';
-import { ProfileComponent } from './modules/prontuario/pages/profile/profile.component';
-import { AnamneseComponent } from './modules/prontuario/pages/anamnese/anamnese.component';
-import { EventosComponent } from './modules/prontuario/pages/eventos/eventos.component';
-import { HistoricoClinicoComponent } from './modules/prontuario/pages/historico-clinico/historico-clinico.component';
-import { GestaoDeUsuariosComponent } from './modules/prontuario/pages/gestao-de-usuarios/gestao-de-usuarios.component';
-import { ProntuarioModule } from './modules/prontuario/prontuario.module';
-import { CheckboxComponent } from './components/checkbox/checkbox.component';
-import { ButtonComponent } from './components/button/button.component';
-import { ContainerComponent } from './components/container/container.component';
-import { CardComponent } from './components/card/card.component';
-import { DatepickerComponent } from './components/datepicker/datepicker.component';
-import { DialogComponent } from './components/dialog/dialog.component';
-import { DragDropComponent } from './components/drag-drop/drag-drop.component';
-import { HorizontalInfoComponent } from './components/horizontal-info/horizontal-info.component';
-import { InputComponent } from './components/input/input.component';
-import { ListComponent } from './components/list/list.component';
-import { MenuComponent } from './components/menu/menu.component';
-import { PageHeaderComponent } from './components/page-header/page-header.component';
-import { RadioComponent } from './components/radio/radio.component';
-import { SelectComponent } from './components/select/select.component';
-import { SideBarComponent } from './components/side-bar/side-bar.component';
-import { TableComponent } from './components/table/table.component';
-import { TabsComponent } from './components/tabs/tabs.component';
-import { ToolbarComponent } from './components/toolbar/toolbar.component';
-import { TreeComponent } from './components/tree/tree.component';
-import { VerticalInfoComponent } from './components/vertical-info/vertical-info.component';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { TableListComponent } from './table-list/table-list.component';
+import { TypographyComponent } from './typography/typography.component';
+import { IconsComponent } from './icons/icons.component';
+import { MapsComponent } from './maps/maps.component';
+import { NotificationsComponent } from './notifications/notifications.component';
+import { UpgradeComponent } from './upgrade/upgrade.component';
+import {
+  AgmCoreModule
+} from '@agm/core';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+
 @NgModule({
+  imports: [
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule,
+    ComponentsModule,
+    RouterModule,
+    AppRoutingModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'YOUR_GOOGLE_MAPS_API_KEY'
+    })
+  ],
   declarations: [
     AppComponent,
-    LoginComponent,
-    HomeComponent,
-    NavComponent,
-    ProfileComponent,
-    AnamneseComponent,
-    EventosComponent,
-    HistoricoClinicoComponent,
-    GestaoDeUsuariosComponent,
-    CheckboxComponent,
-    ButtonComponent,
-    ContainerComponent,
-    CardComponent,
-    DatepickerComponent,
-    DialogComponent,
-    DragDropComponent,
-    HorizontalInfoComponent,
-    InputComponent,
-    ListComponent,
-    MenuComponent,
-    PageHeaderComponent,
-    RadioComponent,
-    SelectComponent,
-    SideBarComponent,
-    TableComponent,
-    TabsComponent,
-    ToolbarComponent,
-    TreeComponent,
-    VerticalInfoComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ProntuarioModule
+    AdminLayoutComponent,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
