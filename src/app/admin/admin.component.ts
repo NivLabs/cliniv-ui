763d2b20bf6a2ac383aaa15eb94e75a207c71cb9
viniciosarodrigues <viniciosarodrigues@gmail.com>
2020-04-05 10:04:31 -0300

    Criando tela de Administrador

---
+++ src/app/admin/admin.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin',
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.css']
+})
+export class AdminComponent implements OnInit {
+
+  public loading: boolean;
+  public userNotFound: boolean;
+  users = [];
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
