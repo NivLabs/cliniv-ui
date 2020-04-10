e9b52230c73ae8eb38b4ad7a8694a878f7339170
viniciosarodrigues <viniciosarodrigues@gmail.com>
2020-04-09 23:17:41 -0300

    Cirando consultas paginadas e filtradas

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ErrorHandlerService } from 'app/core/error-handler.service';
 import { NotificationsComponent } from 'app/core/notification/notifications.component';
 import { AdminService } from './admin.service';
-import { Page } from 'app/model/Util';
+import { Page, Pageable } from 'app/model/Util';
 
 @Component({
   selector: 'app-admin',
@@ -13,17 +13,19 @@ export class AdminComponent implements OnInit {
 
   public loading: boolean;
   public userNotFound: boolean;
+  loadedUsers: []
   page: Page;
+  pageSettings: Pageable;
+  filters: any;
 
-  constructor(private userService: AdminService, private errorHandler: ErrorHandlerService, private notification: NotificationsComponent) {
-    this.page = new Page();
-    this.page.content = [];
-  }
+  constructor(private userService: AdminService, private errorHandler: ErrorHandlerService, private notification: NotificationsComponent) { }
 
   ngOnInit(): void {
+    this.pageSettings = new Pageable();
     this.loading = true;
-    this.userService.getPage(null).then(response => {
+    this.userService.getPage(this.filters, this.pageSettings).then(response => {
       this.loading = false;
+      this.loadedUsers = response.content;
       this.page = response;
       this.userNotFound = this.page.content.length === 0;
     }).catch(error => {
@@ -33,7 +35,31 @@ export class AdminComponent implements OnInit {
     });
   }
 
-  openDialog(id) {
+  /**
+   * Realiza a paginação dos componentes
+   */
+  loadNextPage() {
+    if (this.page && !this.page.last) {
+      this.loading = true;
+      this.pageSettings.page = this.pageSettings.page + 1;
+      this.userService.getPage(this.filters, this.pageSettings).then(response => {
+        this.loading = false;
+        response.content.forEach(newItem => {
+          this.loadedUsers.push(newItem);
+        })
+        this.page = response;
+      }).catch(error => {
+        this.userNotFound = this.page.content !== undefined ? this.page.content.length === 0 : true;
+        this.loading = false;
+        this.errorHandler.handle(error, null);
+      })
+    }
+  }
 
+  /**
+   * 
+   * @param id Identificador do usuário
+   */
+  openDialog(id) {
   }
 }
