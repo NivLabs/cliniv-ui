d1c5091c73d7211623ee060a2e5614a8aa5356d8
viniciosarodrigues <viniciosarodrigues@gmail.com>
2020-04-09 23:17:24 -0300

    Correção da entidade de paginação

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: "https://gestao-prontuario.herokuapp.com",
+  apiUrl: "http://localhost:8080",//"https://gestao-prontuario.herokuapp.com",
   tokenWhitelistedDomains: [],
   tokenBlacklistedRoutes: []
 };
