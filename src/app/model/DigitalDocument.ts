35d6fcf342d7b120c7a11bce03c41ac5a0598287
viniciosarodrigues <viniciosarodrigues@gmail.com>
2020-05-09 18:30:59 -0300

    feat: add. model para Documentos Digitais

---
+++ src/app/model/DigitalDocument.ts
@@ -0,0 +1,14 @@
+/**
+ * Classe que representa documentos digitais da aplicação
+ */
+export class DigitalDocument {
+    constructor() {
+        this.type = 'PDF';
+        this.name = 'Nome do documento';
+    }
+    id: number;
+    type: string; // PDF, JPEG, PNG, XML, JSON
+    base64: string;
+    name: string;
+    createdAt: Date;
+}
