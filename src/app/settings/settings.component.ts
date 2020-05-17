73df06cfde960315327e91ef2450f1b31abb1996
JairRibeiro <jairribeirofilho@gmail.com>
2020-05-16 21:43:04 -0300

    updt: Consulta de cep

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -3,6 +3,8 @@ import { ErrorHandlerService } from 'app/core/error-handler.service';
 import { SeetingsInfo } from 'app/model/Settings';
 import { SettingsService } from 'app/settings/settings.service';
 import { MatTableDataSource } from '@angular/material/table';
+import { NotificationsComponent } from 'app/core/notification/notifications.component';
+import { AddressService } from 'app/core/address.service';
 
 @Component({
   selector: 'app-settings',
@@ -17,7 +19,7 @@ export class SettingsComponent implements OnInit {
   public dataSource: any;
   public displayedColumns: any;
 
-  constructor(private principalService: SettingsService, private errorHandler: ErrorHandlerService) { }
+  constructor(private principalService: SettingsService, private errorHandler: ErrorHandlerService, private addressService: AddressService, private notification: NotificationsComponent) { }
 
   ngOnInit(){
     this.settings = new SeetingsInfo();
@@ -38,6 +40,20 @@ export class SettingsComponent implements OnInit {
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  searchAddressByCEP() {
+    this.loading = true;
+    this.addressService.getAddressByCep(this.settings.customerInfo.address.postalCode).then(address => {
+      this.loading = false;
+      this.settings.customerInfo.address.city = address.localidade;
+      this.settings.customerInfo.address.neighborhood = address.bairro;
+      this.settings.customerInfo.address.state = address.uf;
+      this.settings.customerInfo.address.street = address.logradouro;
+    }).catch(error => {
+      this.loading = false;
+      this.notification.showWarning("Não foi possível realizar a busca do CEP, verifique se o mesmo está correto.")
+    });
   }
 
 }
