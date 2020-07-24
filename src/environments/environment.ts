a782352d77613227ec91cd9ae4d724ea9573e5c5
JairRibeiro <jairribeirofilho@gmail.com>
2020-07-23 21:23:26 -0300

    #23 Espaçamento entre cards

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: "http://localhost:8080",
+  apiUrl: "https://gestao-prontuario.herokuapp.com",
   tokenWhitelistedDomains: [],
   tokenBlacklistedRoutes: []
 };
