3e26750755426574f3cb969aa5d48abb0a68c0d7
Vinícios Rodrigues <viniciosarodrigues@gmail.com>
2020-09-06 18:51:28 -0300

    [feat] Add filtragem e paginação de convênios

--- src/app/convenant/convenant.component.ts
+++ src/app/convenant/convenant.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ConvenantFilter } from 'app/model/Convenant';
+import { Page, Pageable } from 'app/model/Util';
+import { ConvenantService } from './convenant.service';
+import { ErrorHandlerService } from 'app/core/error-handler.service';
 
 @Component({
   selector: 'app-convenant',
@@ -8,11 +11,31 @@ import { ConvenantFilter } from 'app/model/Convenant';
 })
 export class ConvenantComponent implements OnInit {
 
-  private filters = ConvenantFilter;
+  public loading: boolean;
+  public dataNotFound: boolean;
+  private datas: [];
+  private page: Page;
+  private pageSettings: Pageable;
+  private filters: ConvenantFilter;
 
-  constructor() { }
+  constructor(private principalService: ConvenantService, private errorHandler: ErrorHandlerService) { }
 
   ngOnInit(): void {
+    this.loading = true;
+    this.page = new Page();
+    this.filters = new ConvenantFilter();
+    this.pageSettings = new Pageable();
+    this.principalService.getPage(this.filters, this.pageSettings).then(response => {
+      this.loading = false;
+      this.datas = response.content;
+      this.page = response;
+      this.dataNotFound = this.datas.length === 0;
+      console.log(this.dataNotFound);
+    }).catch(error => {
+      this.dataNotFound = this.datas ? this.datas.length === 0 : true;
+      this.loading = false;
+      this.errorHandler.handle(error, null);
+    });
   }
 
 
@@ -22,7 +45,42 @@ export class ConvenantComponent implements OnInit {
     }
   }
 
-  applyFilter() {
+  /**
+   * Realiza a paginação dos componentes
+   */
+  loadNextPage() {
+    if (this.page && !this.page.last) {
+      this.loading = true;
+      this.pageSettings.page = this.pageSettings.page + 1;
+      this.principalService.getPage(this.filters, this.pageSettings).then(response => {
+        this.loading = false;
+        response.content.forEach(newItem => {
+          this.datas.push(newItem);
+        })
+        this.page = response;
+      }).catch(error => {
+        this.loading = false;
+        this.errorHandler.handle(error, null);
+      })
+    }
+  }
 
+
+  applyFilter() {
+    if (this.filters) {
+      this.loading = true;
+      this.pageSettings = new Pageable();
+      this.principalService.getPage(this.filters, this.pageSettings).then(response => {
+        this.loading = false;
+        this.datas = response.content;
+        this.page = response;
+        this.dataNotFound = this.datas.length === 0;
+        console.log(this.dataNotFound);
+      }).catch(error => {
+        this.dataNotFound = this.datas ? this.datas.length === 0 : true;
+        this.loading = false;
+        this.errorHandler.handle(error, null);
+      });
+    }
   }
 }
