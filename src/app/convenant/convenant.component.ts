7fc4ad4a35f614f39b08e641c90f9c0a2ce12aa5
Vinícios Rodrigues <viniciosarodrigues@gmail.com>
2020-09-06 18:39:56 -0300

    [feat] Estrutura base de filtros de convênios

--- src/app/convenant/convenant.component.ts
+++ src/app/convenant/convenant.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ConvenantFilter } from 'app/model/Convenant';
 
 @Component({
   selector: 'app-convenant',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConvenantComponent implements OnInit {
 
+  private filters = ConvenantFilter;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
+  enterKeyPress(event: any) {
+    if (event.key === "Enter") {
+      this.applyFilter();
+    }
+  }
+
+  applyFilter() {
+
+  }
 }
---
+++ src/app/model/Convenant.ts
@@ -0,0 +1,5 @@
+
+export class ConvenantFilter {
+    id: string;
+    description: string;
+}
