b1b95c3828562afc1130c66a261f2b2807b133f6
Vinícios Rodrigues <viniciosarodrigues@gmail.com>
2020-09-07 21:19:27 -0300

    [feat] Evolução de procedimentos

--- src/app/model/Procedure.ts
+++ src/app/model/Procedure.ts
@@ -1,10 +1,16 @@
 /**
  * Classe que representa as informações de um procedimento
  */
-export class ProcedureInfo {   
+export class ProcedureInfo {
     id: number;
     description: string;
     active: boolean;
+    previousAudit: boolean;
+    specialAuthorization: boolean;
+    specialty: boolean;
+    maxAge: string;
+    minAge: string;
+    frequency: string;
 }
 
 /**
