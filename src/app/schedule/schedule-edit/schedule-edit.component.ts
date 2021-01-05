68783bd94438c859f33ed15d494f5cccc0d162f8
Vinícios Rodrigues <viniciosarodrigues@gmail.com>
2021-01-04 22:30:11 -0300

    [feat] Schedule - Pesquisa de paciente

--- src/app/schedule/schedule-edit/schedule-edit.component.ts
+++ src/app/schedule/schedule-edit/schedule-edit.component.ts
@@ -2,8 +2,14 @@ import { DatePipe } from '@angular/common';
 import { Component, Inject, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ErrorHandlerService } from 'app/core/error-handler.service';
 import { NotificationsComponent } from 'app/core/notification/notifications.component';
+import { UtilService } from 'app/core/util.service';
+import { Address } from 'app/model/Address';
+import { Document } from 'app/model/Document';
+import { PatientInfo } from 'app/model/Patient';
 import { ScheduleInfo } from 'app/model/Schedule';
+import { PatientService } from 'app/patient/patient.service';
 import * as moment from 'moment';
 
 @Component({
@@ -22,6 +28,9 @@ export class ScheduleEditComponent implements OnInit {
   constructor(
     private dialogRef: MatDialogRef<ScheduleEditComponent>,
     private notification: NotificationsComponent,
+    private utilService: UtilService,
+    private patientService: PatientService,
+    private errorHandler: ErrorHandlerService,
     @Inject(MAT_DIALOG_DATA) public data: any
   ) { }
 
@@ -44,7 +53,69 @@ export class ScheduleEditComponent implements OnInit {
   }
 
   enterKeyPress(event, type) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      switch (type) {
+        case "ID":
+          this.searchPatientById();
+          break;
+        case "CPF":
+          this.searchPatientByCpf();
+          break;
+        default:
+          this.notification.showError('Tipo de consulta não encontrado!');
+          break;
+      }
+    }
 
+  }
+
+
+  cpfIsValid() {
+    if (this.dataToForm.patient.document) {
+      if (this.dataToForm.patient.document.value === "" || this.dataToForm.patient.document.value === undefined)
+        return true
+      return this.utilService.cpfIsValid(this.dataToForm.patient.document.value);
+    }
+    return false
+  }
+
+  searchPatientByCpf() {
+    if (!this.cpfIsValid()) {
+      this.notification.showWarning("CPF Inválido, favor informar um CPF válido e sem pontos e/ou traços");
+      this.dataToForm.patient.document = new Document("CPF");
+    } else {
+      this.loading = true;
+      this.patientService.getByDocument('CPF', this.dataToForm.patient.document.value).then(resp => {
+        this.loading = false;
+        this.dataToForm.patient = resp;
+        if (!resp.address) {
+          this.dataToForm.patient.address = new Address();
+        }
+      }).catch(error => {
+        this.loading = false;
+        this.dataToForm.patient.document = new Document('CPF');
+        this.errorHandler.handle(error, this.dialogRef);
+      });
+    }
+  }
+
+  searchPatientById() {
+    this.loading = true;
+    this.patientService.getById(this.dataToForm.patient.id).then(resp => {
+      this.loading = false;
+      this.dataToForm.patient = resp;
+      if (!resp.address) {
+        this.dataToForm.patient.address = new Address();
+      }
+      if (!resp.document) {
+        this.dataToForm.patient.document = new Document('CPF');
+      }
+    }).catch(error => {
+      this.loading = false;
+      this.dataToForm.patient = new PatientInfo();
+      this.errorHandler.handle(error, this.dialogRef);
+    });
   }
 
   save() {
