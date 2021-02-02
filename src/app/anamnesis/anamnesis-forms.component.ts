4d8929c9a9444b5ffd22ddcff04d9bc23221402c
Vinícios Rodrigues <viniciosarodrigues@gmail.com>
2021-02-01 22:57:22 -0300

    [feat] Base para formulários de Anamnese

---
+++ src/app/anamnesis/anamnesis-forms.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-anamnesis-forms',
+  templateUrl: './anamnesis-forms.component.html',
+  styleUrls: ['./anamnesis-forms.component.css']
+})
+export class AnamnesisFormsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
