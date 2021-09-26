c9c2f9092be09fe732b0d619baf3becad28a9e29
Vinícios Rodrigues <viniciosarodrigues@gmail.com>
2021-09-26 12:29:15 -0300

    [bug] Listagem de parâmetros novo relatório

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
+
 export const environment = {
    production: false,
-   apiUrl: 'https://gestao-prontuario.herokuapp.com',
-   appVersion: '1.0.1'
+   apiUrl: 'http://localhost:8080',
+   appVersion: '1.0.1-dev'
 };
