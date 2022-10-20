ce757544ae92b09ce5581d91695e212370d3c359
Vinícios Rodrigues <vinicios.araujo@linx.com.br>
2022-10-20 00:26:25 -0300

    [feat] Ajuste de ordenação

---
+++ src/app/core/CustomPaginatorIntl.ts
@@ -0,0 +1,27 @@
+import {Component, Injectable, NgModule} from '@angular/core';
+import {MatPaginatorIntl, MatPaginatorModule} from '@angular/material/paginator';
+import {Subject} from 'rxjs';
+
+@Injectable()
+export class CustomPaginatorIntl implements MatPaginatorIntl {
+  changes = new Subject<void>();
+
+  // For internationalization, the `$localize` function from
+  // the `@angular/localize` package can be used.
+  firstPageLabel = $localize`Primeira págin a`;
+  itemsPerPageLabel = $localize`Itens por página:`;
+  lastPageLabel = $localize`Última página`;
+
+  // You can set labels to an arbitrary string too, or dynamically compute
+  // it through other third-party internationalization libraries.
+  nextPageLabel = 'Próxima página';
+  previousPageLabel = 'Página anterior';
+
+  getRangeLabel(page: number, pageSize: number, length: number): string {
+    if (length === 0) {
+      return $localize`Página 1 de 1`;
+    }
+    const amountPages = Math.ceil(length / pageSize);
+    return $localize`Página ${page + 1} de ${amountPages}`;
+  }
+}
