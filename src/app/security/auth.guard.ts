eb8e50ff5d6ad702c7b9d0b83bcb3b1fb2dba94f
Vinícios Rodrigues <viniciosarodrigues@gmail.com>
2024-09-04 16:50:42 -0300

    Migração Angular 16

--- src/app/security/auth.guard.ts
+++ src/app/security/auth.guard.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
 
 import { AuthService } from './auth.service';
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
 
   constructor(
     private auth: AuthService,
